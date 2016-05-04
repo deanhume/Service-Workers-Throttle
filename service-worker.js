fec80a8ddb00e7345da183fa58ca6eaf85bd3dcc
Dean <deanhume@gmail.com>
2016-05-04 12:06:02 +0100

    Almost there

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,44 @@
 'use strict';
 
 // A function to throttle any fetch events
-function debounce(inner, ms = 0) {
-  let timer = null;
-  let resolves = [];
-
-  return function (...args) {
-    // Run the function after a certain amount of time
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      // Get the result of the inner function, then apply it to the resolve function of
-      // each promise that has been created since the last time the inner function was run
-      let result = inner(...args);
-      resolves.forEach(r => r(result));
-      resolves = [];
-    }, ms);
-
-    return new Promise(r => resolves.push(r));
-  };
+function timeout(fn, delay) {
+return new Promise(function(resolve, reject) {
+    var timer = null;
+    return function () {
+      console.log('timeout');
+      console.log(arguments);
+      var context = this, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        resolve(fn.apply(context, args));
+      }, delay);
+    };
+  });
 }
 
-
 self.addEventListener('install', function(event) {
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
-    if (self.clients && clients.claim) {
-        clients.claim();
-    }
+  if (self.clients && clients.claim) {
+    clients.claim();
+  }
 });
+
 
 self.addEventListener('fetch', function(event) {
-
-   event.respondWith(debounce(fetch(event.request), 2000));
+  timeout(() => fetch(event.request), 2000).then(function(value) {
+    console.log(value);
+  });
 });
+
+// self.addEventListener('fetch', function(event) {
+//
+//    //event.respondWith(debounce(fetch(event.request), 2000));
+//    event.respondWith(debounce(() => fetch(event.request)
+//                                     .then(function (resp) {
+//                                       console.log(resp);
+//                         return resp.response;
+//                     }), 2000));
+// });
